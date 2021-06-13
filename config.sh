0fe3a89432582f33b18209d9b2dbbaf9e2af6eb2
Bensuperpc <bensuperpc@gmail.com>
2021-06-13 18:07:48 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,13 +4,13 @@ OLD_IMAGE_NAME="<<IMAGE_NAME>>"
 NEW_IMAGE_NAME="$1"
 find . -type f -name "*" ! -path ".git" ! -path "$0" -print0 | xargs -0 sed -i "s/$OLD_IMAGE_NAME/$NEW_IMAGE_NAME/g"
 
-if [ ! $2 -eq 0 ]
+if [ ! -z "$2" ]; then
     OLD_COMPILER_NAME="<<IMAGE_COMPILER>>"
     NEW_COMPILER_NAME="$2"
     find . -type f -name "*" ! -path ".git" ! -path "$0" -print0 | xargs -0 sed -i "s/$OLD_COMPILER_NAME/$NEW_COMPILER_NAME/g"
 fi
 
-if [ ! $3 -eq 0 ]
+if [ ! -z "$3" ]; then
     OLD_GIT_URL="<<GIT_URL>>"
     NEW_GIT_URL="$3"
     find . -type f -name "*" ! -path ".git" ! -path "$0" -print0 | xargs -0 sed -i "s/$OLD_GIT_URL/$NEW_GIT_URL/g"
